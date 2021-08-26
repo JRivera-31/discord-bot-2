33078bfb85f42bb8d649c8e633a45454d2cda710
JRivera-31 <iijrivera31@gmail.com>
2021-08-25 20:36:23 -0400

    Added cog

--- piss_bot.py
+++ piss_bot.py
@@ -1,3 +1,4 @@
+from weakref import finalize
 import discord
 import os
 
@@ -11,5 +12,20 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-client = commands.Bot(command_prefix="pp!")
+# Set Prefix for commands
+client = commands.Bot(command_prefix="pp!", self_bot=False)
 
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+# To run the bot
+client.run(BOT_TOKEN)
